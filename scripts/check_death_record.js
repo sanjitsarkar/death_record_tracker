da8652b5f0bfc975faa959c3130cdea2fdd20e55
Xanjit Xarkar <ssanjit856@gmail.com>
2021-06-20 21:07:45 +0530

    fixed approve and resubmit button visibility

--- scripts/check_death_record.js
+++ scripts/check_death_record.js
@@ -88,83 +88,171 @@ const content = deathRecord.querySelector('ul')
 
     if(querySnapshot.docs.length)
     {
-        console.log(querySnapshot.docs.length)
+        // console.log(querySnapshot.docs.length)
     querySnapshot.forEach((doc) => {
 
-        content.innerHTML+=`<li>
-                <div class="collapsible-header">${doc.data().name}</div>
-                <div class="collapsible-body">
-                  <form id="death-record-form">
-                    <div class="input-field inline">
-                   <p>I hereby certify that the 
-                    <input type="text" name="title" id="title" value=${doc.data().title} placeholder="Title" maxlength="200"  readonly>
-          
-                    </div>
-                     <input type="text" name="name" id="name" value=${doc.data().name} placeholder="Name" maxlength="200" readonly>
-                     <div class="input-field inline">
-                      <input type="text" name="relation" id="relation" value=${doc.data().relation} placeholder="Relation" maxlength="200" readonly>
-                    
-                 
-                    
-                    of
-                  </div>
-                    <input type="text" name="relative_name" id="relative_name" value=${doc.data().relative_name} placeholder="Relative maxlength="200" name" readonly>
-                    resident of <input type="text" name="resident" id="resident" value=${doc.data().resident} placeholder="Resident"maxlength="200"  readonly>
-                    was under my treatment from <input type="text" value=${doc.data().from} name="from" id="from" placeholder="From"maxlength="200"  readonly>
-                    to <input type="text" name="to" id="to" value=${doc.data().to} placeholder="To" maxlength="200" readonly>
-                    and he/she died on <input type="text" name="date" value=${doc.data().date} id="date" maxlength="200" readonly>
-                    at <input type="text" name="time" value=${doc.data().time} id="time" maxlength="200" readonly>
-                 
-                    <h6>Resaon of death</h6>
-                    <input type="text" name="reason" id="reason" value=${doc.data().reason} placeholder="Resaon of death" maxlength="200"  readonly>
-                    <h6>Age</h6>
-                   
-                    <input type="number" id="age" name="age" value=${doc.data().age} required />
-                    <h6>Sex</h6>
-                   
-                    <input type="text" id="sex" name="sex" value=${doc.data().sex} required />
-                 
-                    
-                   </p>
-  
-               
-              
-                   <button class="btn green darken-2 z-depth-0 approve" value="" style="display:none">Approve</button>
-                   <button class="btn red darken-2 z-depth-0 resubmit modal-trigger"  data-target="modal-resubmit-reason" value="" style="display:none">Resubmit</button>
-               
-                   </form>
-                
-               
-                </div>
-              </li>
-            </ul>`
        
+       
+        console.log(doc.data)
+        if(doc.data().isResubmit)
+        {
+          
+        content.innerHTML+=`<li>
+        <div class="collapsible-header">${doc.data().name}</div>
+        <div class="collapsible-body">
+          <form id="death-record-form">
+            <div class="input-field inline">
+           <p>I hereby certify that the 
+            <input type="text" name="title" id="title" value=${doc.data().title} placeholder="Title" maxlength="200"  readonly>
+  
+            </div>
+             <input type="text" name="name" id="name" value=${doc.data().name} placeholder="Name" maxlength="200" readonly>
+             <div class="input-field inline">
+              <input type="text" name="relation" id="relation" value=${doc.data().relation} placeholder="Relation" maxlength="200" readonly>
+            
+         
+            
+            of
+          </div>
+            <input type="text" name="relative_name" id="relative_name" value=${doc.data().relative_name} placeholder="Relative maxlength="200" name" readonly>
+            resident of <input type="text" name="resident" id="resident" value=${doc.data().resident} placeholder="Resident"maxlength="200"  readonly>
+            was under my treatment from <input type="text" value=${doc.data().from} name="from" id="from" placeholder="From"maxlength="200"  readonly>
+            to <input type="text" name="to" id="to" value=${doc.data().to} placeholder="To" maxlength="200" readonly>
+            and he/she died on <input type="text" name="date" value=${doc.data().date} id="date" maxlength="200" readonly>
+            at <input type="text" name="time" value=${doc.data().time} id="time" maxlength="200" readonly>
+         
+            <h6>Resaon of death</h6>
+            <input type="text" name="reason" id="reason" value=${doc.data().reason} placeholder="Resaon of death" maxlength="200"  readonly>
+            <h6>Age</h6>
+           
+            <input type="number" id="age" name="age" value=${doc.data().age} required />
+            <h6>Sex</h6>
+           
+            <input type="text" id="sex" name="sex" value=${doc.data().sex} required />
+         
+            
+           </p>
+
+       
+      
+           <button class="btn green darken-2 z-depth-0 approve" value="" style="display:inline">Approve</button>
+          
+       
+           </form>
+        
+       
+        </div>
+      </li>
+    </ul>`
+    
+        }
+        else if(doc.data().isApproved){
+          
+        content.innerHTML+=`<li>
+        <div class="collapsible-header">${doc.data().name}</div>
+        <div class="collapsible-body">
+          <form id="death-record-form">
+            <div class="input-field inline">
+           <p>I hereby certify that the 
+            <input type="text" name="title" id="title" value=${doc.data().title} placeholder="Title" maxlength="200"  readonly>
+  
+            </div>
+             <input type="text" name="name" id="name" value=${doc.data().name} placeholder="Name" maxlength="200" readonly>
+             <div class="input-field inline">
+              <input type="text" name="relation" id="relation" value=${doc.data().relation} placeholder="Relation" maxlength="200" readonly>
+            
+         
+            
+            of
+          </div>
+            <input type="text" name="relative_name" id="relative_name" value=${doc.data().relative_name} placeholder="Relative maxlength="200" name" readonly>
+            resident of <input type="text" name="resident" id="resident" value=${doc.data().resident} placeholder="Resident"maxlength="200"  readonly>
+            was under my treatment from <input type="text" value=${doc.data().from} name="from" id="from" placeholder="From"maxlength="200"  readonly>
+            to <input type="text" name="to" id="to" value=${doc.data().to} placeholder="To" maxlength="200" readonly>
+            and he/she died on <input type="text" name="date" value=${doc.data().date} id="date" maxlength="200" readonly>
+            at <input type="text" name="time" value=${doc.data().time} id="time" maxlength="200" readonly>
+         
+            <h6>Resaon of death</h6>
+            <input type="text" name="reason" id="reason" value=${doc.data().reason} placeholder="Resaon of death" maxlength="200"  readonly>
+            <h6>Age</h6>
+           
+            <input type="number" id="age" name="age" value=${doc.data().age} required />
+            <h6>Sex</h6>
+           
+            <input type="text" id="sex" name="sex" value=${doc.data().sex} required />
+         
+            
+           </p>
+
+       
+      
+          
+       
+           </form>
+        
+       
+        </div>
+      </li>
+    </ul>`
+            
+        }
+       else{
+ 
+          content.innerHTML+=`<li>
+          <div class="collapsible-header">${doc.data().name}</div>
+          <div class="collapsible-body">
+            <form id="death-record-form">
+              <div class="input-field inline">
+             <p>I hereby certify that the 
+              <input type="text" name="title" id="title" value=${doc.data().title} placeholder="Title" maxlength="200"  readonly>
+    
+              </div>
+               <input type="text" name="name" id="name" value=${doc.data().name} placeholder="Name" maxlength="200" readonly>
+               <div class="input-field inline">
+                <input type="text" name="relation" id="relation" value=${doc.data().relation} placeholder="Relation" maxlength="200" readonly>
+              
+           
+              
+              of
+            </div>
+              <input type="text" name="relative_name" id="relative_name" value=${doc.data().relative_name} placeholder="Relative maxlength="200" name" readonly>
+              resident of <input type="text" name="resident" id="resident" value=${doc.data().resident} placeholder="Resident"maxlength="200"  readonly>
+              was under my treatment from <input type="text" value=${doc.data().from} name="from" id="from" placeholder="From"maxlength="200"  readonly>
+              to <input type="text" name="to" id="to" value=${doc.data().to} placeholder="To" maxlength="200" readonly>
+              and he/she died on <input type="text" name="date" value=${doc.data().date} id="date" maxlength="200" readonly>
+              at <input type="text" name="time" value=${doc.data().time} id="time" maxlength="200" readonly>
+           
+              <h6>Resaon of death</h6>
+              <input type="text" name="reason" id="reason" value=${doc.data().reason} placeholder="Resaon of death" maxlength="200"  readonly>
+              <h6>Age</h6>
+             
+              <input type="number" id="age" name="age" value=${doc.data().age} required />
+              <h6>Sex</h6>
+             
+              <input type="text" id="sex" name="sex" value=${doc.data().sex} required />
+           
+              
+             </p>
+
+             <button class="btn green darken-2 z-depth-0 approve" value="" style="display:inline">Approve</button>
+             <button class="btn red darken-2 z-depth-0 resubmit modal-trigger"  data-target="modal-resubmit-reason" value="" style="display:inline">Resubmit</button>
+        
+             
+         
+             </form>
+          
+         
+          </div>
+        </li>
+      </ul>`
+    
+
+        }
         var approve = document.querySelector('.approve')
         var resubmit = document.querySelector('.resubmit')
         var resubmit_btn = document.querySelector('.resubmit_btn')
         var resubmission_reason_form = document.querySelector('#resubmission_reason_form')
-      
-        if(doc.data().isResubmit)
-        {
-            resubmit.style.display = "none"
-    
-        }
-        else{
-            resubmit.style.display = "inline"
-            
-        }
-        if(doc.data().isApproved)
-        {
-    approve.style.display = "none"
-    resubmit.style.display = "none"
-    
-
-        }
-        else{
-            approve.style.display = "inline"
-    
-        }
-        approve.addEventListener('click',(e)=>
+        approve?.addEventListener('click',(e)=>
         { 
             e.preventDefault()
             approve.disabled = true
@@ -194,7 +282,7 @@ const content = deathRecord.querySelector('ul')
             
     
         })
-        resubmission_reason_form.addEventListener('submit',(e)=>
+        resubmission_reason_form?.addEventListener('submit',(e)=>
         {
             e.preventDefault()
             var resubmissionReason = document.querySelector('#resubmission_reason').value
